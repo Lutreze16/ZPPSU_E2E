d700e48c14a205d8f85d7b4d3750076024d4859d
Lutreze Hue Jacinto <86185117+Lutreze16@users.noreply.github.com>
2024-01-05 14:55:52 +0800

    Update login_student.php

--- login_student.php
+++ login_student.php
@@ -45,82 +45,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
     <title>Login | For Students</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f2f2f2;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-        }
-
-        .container {
-            background-color: #ffffff;
-            padding: 20px;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-
-        h2 {
-            color: #333333;
-            text-align: center;
-        }
-
-        form {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-
-        input,
-        select,
-        button {
-            margin: 10px;
-            padding: 8px;
-            width: 100%;
-            box-sizing: border-box;
-        }
-
-        input,
-        select {
-            border: 1px solid #ccc;
-            border-radius: 3px;
-            height: 30px;
-        }
-
-        button {
-            background-color: maroon;
-            color: #ffffff;
-            cursor: pointer;
-            border: none;
-            border-radius: 3px;
-            height: 40px;
-            font-size: 16px;
-        }
-
-        button:hover {
-            background-color: #800000;
-        }
-
-        p {
-            color: #ff0000;
-            text-align: center;
-            margin-top: 10px;
-        }
-    </style>
 </head>
 <body>
     <div class="container">
         <h2>Student Login</h2>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <input type="number" name="student_id" placeholder="Student ID No." required>
-            <br>
             <input type="password" name="password" placeholder="Password" required>
-            <br>
             <button type="submit">Login</button>
         </form>
         <?php if (!empty($errorMessage)) echo "<p>$errorMessage</p>"; ?>
